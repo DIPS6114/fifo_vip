fd5fe8e3e3ca79fd167cb657e6fe9f02d5ba9d38
shweta.mbytes <shwetapatil@mirafra.com>
2022-07-21 22:21:12 +0530

    added fifo files

---
+++ sim/fifo_compile.f
@@ -0,0 +1,12 @@
++incdir+../../fifo_tb_files/
+../../fifo_tb_files/testbench_top.sv
+../../fifo_tb_files/write_fifo_seq_item.sv
+../../fifo_tb_files/write_fifo_sequence.sv
+../../fifo_tb_files/write_fifo_sequencer.sv
+../../fifo_tb_files/write_fifo_interface.sv
+../../fifo_tb_files/write_fifo_test.sv
+../../fifo_tb_files/write_fifo_environment.sv
+../../fifo_tb_files/write_fifo_agent.sv
+../../fifo_tb_files/write_fifo_driver.sv
+../../fifo_tb_files/write_fifo_monitor.sv
+
