2a14cd7ac6afb20f225d5eee353afdc84574e7bc
Deepak Hemu Rathod <105357054+DIPS6114@users.noreply.github.com>
2022-07-22 15:04:24 +0530

    Update write_fifo_monitor.sv

--- fifo_tb_files/write_fifo_monitor.sv
+++ fifo_tb_files/write_fifo_monitor.sv
@@ -1,41 +1,13 @@
-class write_fifo_monitor extends uvm_monitor;
+class fifo_monitor1 extends uvm_monitor;
   
-  `uvm_component_utils(write_fifo_monitor);
+  `uvm_component_utils(fifo_monitor1)
   
-  write_fifo_seq_item pkt;
-  
-  virtual fifo_if intf;
-
-  uvm_analysis_port #(write_fifo_seq_item)item_collected_port;
-
-  function new(string name="write_fifo_monitor",uvm_component parent);
-     super.new(name,parent);
-   
-     item_collected_port=new("item_collected_port",this);
-    
-    endfunction
-    
-
-  
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    uvm_config_db#(virtual fifo_if)::get(this,"","vif",intf);
-       
+  function new(string name="fifo_monitor1",uvm_component parent=null);
+    super.new(name,parent);
   endfunction
   
-   task run_phase(uvm_phase phase);
-     pkt=write_fifo_seq_item::type_id::create("pkt");
-    @(posedge intf.clk);
-     forever begin
-       @(posedge intf.clk);
-       pkt.wr<=intf.wr;
-       pkt.rd<=intf.rd;
-       pkt.data_in<=intf.data_in;
-       item_collected_port.write(pkt);
-       pkt.display("MONITOR_1");
-
-     end
-     endtask
-  endclass
-    
-    
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+  
+endclass
